b10675882d9456a36a6397e2b6e2a3884abf1cad
Aditya <gade2945@gmail.com>
2024-11-27 00:50:53 +0530

    updated

--- src/app/learn/learn.component.ts
+++ src/app/learn/learn.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-learn',
@@ -8,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrl: './learn.component.css'
 })
 export class LearnComponent {
+
+constructor(private router:Router){
+
+}
+
+html(){
+  this.router.navigate(['/Techno1']);
+}
+
+css(){
+  this.router.navigate(['/Techno2']);
+}
+
+javascript(){
+  this.router.navigate(['/Techno3']);
+
+}
+
+typescript(){
+  this.router.navigate(['/Techno4']);
+}
+
+angular(){
+  this.router.navigate(['/Techno5']);
+}
+
+java(){
+  this.router.navigate(['/Techno6']);
+}
 
 }
